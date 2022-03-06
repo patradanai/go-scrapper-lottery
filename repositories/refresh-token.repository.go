b735cf42806be7b7a370d35bee627009896f7a79
digio <=>
2022-03-06 23:08:11 +0700

    add refresh token:

--- repositories/refresh-token.repository.go
+++ repositories/refresh-token.repository.go
@@ -1,1 +1,35 @@
 package repositories
+
+import (
+	"context"
+	"go.mongodb.org/mongo-driver/mongo"
+	"lottery-web-scrapping/models"
+	"lottery-web-scrapping/utils"
+	"time"
+)
+
+type IRefreshTokenRepository interface {
+	CreateOneRefreshToken(token *models.RefreshToken) error
+}
+
+type RefreshTokenRepository struct {
+	BaseRepository
+}
+
+func NewRefreshTokenRepository(c *mongo.Client) IRefreshTokenRepository {
+	return &RefreshTokenRepository{
+		BaseRepository{c},
+	}
+}
+
+func (c *BaseRepository) CreateOneRefreshToken(token *models.RefreshToken) error {
+	refreshTokenCollection := c.Client.Database(utils.LoadEnv("MONGO_DB_NAME")).Collection("refresh_tokens")
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if _, err := refreshTokenCollection.InsertOne(ctx, token); err != nil {
+		return err
+	}
+
+	return nil
+}
